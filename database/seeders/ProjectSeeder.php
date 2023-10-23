47ae005f1cb6f813bc1642f796b974571598eb2e
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-23 16:35:17 +0200

    feat: seeder Users

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 use Faker\Generator as Faker;
 
@@ -20,7 +21,9 @@ class ProjectSeeder extends Seeder
         for ($i = 0; $i < 100; $i++) {
             $post = new Project();
             $post->title = $faker->catchPhrase();
-            $post->content = $faker->catchPhrase();
+            $post->content = $faker->paragraph(3, true);
+            $post->slug = Str::slug($post->title);
+            $post->save();
         }
     }
 }
