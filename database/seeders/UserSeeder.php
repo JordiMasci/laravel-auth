c53d0ea47df4f0e7025a1d8999854b81186ba6ad
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-23 16:46:51 +0200

    feat: new User->userSeed salva tutti i seed in databaseSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,8 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,6 +17,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email= 'admin@email.it';
+        $user->password = Hash::make('password');
+        $user->save();
     }
 }
