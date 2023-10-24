a16963f1c7393027efa52b0d1612104979fc6830
JordiMasci <133970995+JordiMasci@users.noreply.github.com>
2023-10-24 18:14:40 +0200

    feat: index -> admin

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,7 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    @dump($projects)
-</div>
+    <div class="container">
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Slug</th>
+                    <th scope="col">Created at</th>
+                    <th scope="col">Updated at</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->content }}</td>
+                        <td>{{ $project->slug }}</td>
+                        <td>{{ $project->created_at }}</td>
+                        <td>{{ $project->updated_at }}</td>
+                        <td></td>
+                    </tr>
+
+                @empty
+                <td colspan="6"><i>Non ci sono risultati</i></td>
+                @endforelse
+            </tbody>
+        </table>
+        {{ $projects->links('pagination::bootstrap-5') }}
+    </div>
 @endsection
